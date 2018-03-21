b68f760fb4962b90ffadaa3a59027d459d481090
Nathan Wallace <nathan@turbot.com>
2018-03-21 11:39:36 -0400

    Sanitize data by default. Add trace information if available.

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-tconf = require("turbot-config");
-winston = require("winston");
+const tconf = require("turbot-config");
+const utils = require("turbot-utils");
+const winston = require("winston");
+const format = winston.format;
 
-const turbotRewriter = function(level, msg, meta) {
-  let correlationId = tconf.get(["trace", "correlationId"]);
-  // Should not happen, but if no correlationId is given for the request then
-  // we have no choice but to continue. It's not like we can log here!
-  if (!correlationId) {
-    return meta;
+const trace = format(function(info, opts) {
+  let trace = tconf.get("trace");
+  if (trace) {
+    info.trace = trace;
   }
-  // Enrich the log data
-  if (!meta.turbot) {
-    meta.turbot = {};
-  }
-  meta.turbot.correlationId = correlationId;
-  return meta;
-};
+  return info;
+});
 
-const winstonTransportSettings = {
+const sanitize = format(function(info, opts) {
+  return utils.data.sanitize(info, { clone: false });
+});
+
+const logger = winston.createLogger({
   // Turbot uses syslog levels
   levels: winston.config.syslog.levels,
   // Set the log level for all transports
   level: tconf.get(["log", "level"], process.env.LOG_LEVEL || "info"),
   // Errors should be handled elsewhere, not by the log process
-  exitOnError: true,
-  rewriters: [turbotRewriter],
-  // Use console transport, which is great for both Lambda functions and docker
-  // containers - generally where Turbot is run.
+  exitOnError: false,
+  // Formatting specific to Turbot requirements
+  format: format.combine(
+    // Add Turbot trace information to all entries
+    trace(),
+    // Hide sensitive data by default
+    sanitize(),
+    // Add a timestamp to all entries
+    format.timestamp(),
+    // Log as JSON
+    format.json()
+  ),
   transports: [
-    new winston.transports.Console({
-      handleExceptions: true,
-      json: true,
-      stringify: tconf.get(["turbot", "environment"], process.env.NODE_ENV) == "production",
-      timestamp: true
-    })
+    // Use console transport, which is great for both Lambda functions and docker
+    // containers - generally where Turbot is run.
+    new winston.transports.Console({ handleExceptions: true })
   ]
-};
-
-const logger = new winston.Logger(winstonTransportSettings);
+});
 
 module.exports = logger;
--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
+const _ = require("lodash");
 const assert = require("chai").assert;
 const log = require("..");
+const tconf = require("turbot-config");
 const testConsole = require("test-console");
 
 describe("turbot-log", function() {
@@ -7,7 +9,7 @@ describe("turbot-log", function() {
     let output, outputLines;
     let msg = "hello";
     before(function() {
-      outputLines = testConsole.inspectSync(function() {
+      outputLines = testConsole.stdout.inspectSync(function() {
         log.info(msg);
       });
       output = JSON.parse(outputLines[0]);
@@ -26,12 +28,6 @@ describe("turbot-log", function() {
     });
   });
 
-  // TODO - Test correlationId is included
-  // TODO - Test correlationId changes with each request
-  // TODO - Test extra metadata
-  // NOTE - error messages are on stderr (not stdout)
-  // TODO - Test log level can change per request
-
   describe("hide debug if at info level", function() {
     let outputLines;
     let msg = "hello";
@@ -44,4 +40,99 @@ describe("turbot-log", function() {
       assert.lengthOf(outputLines, 0);
     });
   });
+
+  describe("Add trace data if available", function() {
+    let tmpEnv;
+    let trace = {
+      correlationId: "abcd1234"
+    };
+
+    before(function() {
+      tmpEnv = _.pick(process.env, "AWS_REGION", "TURBOT_CONFIG_ENV");
+      process.env.TURBOT_CONFIG_ENV = JSON.stringify({ trace: trace });
+      tconf.$load();
+    });
+
+    after(function() {
+      for (k in tmpEnv) {
+        if (tmpEnv[k]) {
+          process.env[k] = tmpEnv;
+        } else {
+          delete process.env[k];
+        }
+      }
+      tconf.$load();
+    });
+
+    before(function() {
+      outputLines = testConsole.stdout.inspectSync(function() {
+        log.info("hello");
+      });
+      output = JSON.parse(outputLines[0]);
+    });
+    it("has trace data", function() {
+      assert.deepInclude(output, { trace: trace });
+    });
+  });
+
+  describe("Hide sensitive data", function() {
+    let outputLines, output;
+    let data = {
+      host: "localhost",
+      user: "turbot",
+      $password: "secret"
+    };
+    before(function() {
+      outputLines = testConsole.stdout.inspectSync(function() {
+        log.info("here's the data!", data);
+      });
+      output = JSON.parse(outputLines[0]);
+    });
+    it("host is unchanged (not <sensitive>)", function() {
+      assert.equal(output.host, data.host);
+    });
+    it("$password is <sensitive>", function() {
+      assert.equal(output.$password, "<sensitive>");
+    });
+  });
+
+  xdescribe("TODO", function() {
+    it("Set log level per trace.logLevel", function() {});
+    it("Logs but does not absorb exceptions", function() {
+      let outputLines, output, err;
+      // I can't see how to test this ... but it's something like this.
+      try {
+        outputLines = testConsole.stderr.inspectSync(function() {
+          throw new Error("test");
+        });
+      } catch (e) {
+        err = e;
+      }
+      console.log(outputLines);
+      console.log(output);
+      console.log(err);
+    });
+  });
 });
+
+/**
+ * For logging, sometimes it's better & easier to test with a script.
+
+const log = require("./index");
+
+process.on("uncaughtException", function() {
+  console.log("uncaughtException!");
+});
+
+process.on("exit", function() {
+  console.log("exit!");
+});
+
+log.info("hello");
+log.info("hello", { with: "data" });
+log.info("hello", { with: { $sensitive: "data" } });
+log.info("hello", { $sensitiveAtRoot: "sensitive" });
+throw new Error({ message: "test", with: "data" });
+log.info("final");
+
+*/
