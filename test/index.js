a706a849169a8fc012c92465274b8c4e97dd4b8c
Victor Hadianto <victor@turbot.com>
2020-04-30 11:51:45 +1000

    5.1.0. Closes #16.

--- test/index.js
+++ test/index.js
@@ -4,128 +4,128 @@ const errors = require("@turbot/errors");
 const log = require("..");
 const testConsole = require("test-console");
 
-describe("turbot-log", function() {
-  describe("log info by default", function() {
+describe("turbot-log", function () {
+  describe("log info by default", function () {
     let output, outputLines;
     let msg = "hello";
-    before(function() {
-      outputLines = testConsole.stdout.inspectSync(function() {
+    before(function () {
+      outputLines = testConsole.stdout.inspectSync(function () {
         log.info(msg);
       });
       output = JSON.parse(outputLines[0]);
     });
-    it("logged only one message", function() {
+    it("logged only one message", function () {
       assert.lengthOf(outputLines, 1);
     });
-    it("logged at info level", function() {
+    it("logged at info level", function () {
       assert.equal(output.level, "info");
     });
-    it("has correct message", function() {
+    it("has correct message", function () {
       assert.equal(output.message, msg);
     });
-    it("has a timestamp", function() {
+    it("has a timestamp", function () {
       assert.exists(output.timestamp);
     });
   });
 
-  describe("hide debug if at info level", function() {
+  describe("hide debug if at info level", function () {
     let outputLines;
     let msg = "hello";
-    before(function() {
-      outputLines = testConsole.stdout.inspectSync(function() {
+    before(function () {
+      outputLines = testConsole.stdout.inspectSync(function () {
         log.debug(msg);
       });
     });
-    it("logged zero messages", function() {
+    it("logged zero messages", function () {
       assert.lengthOf(outputLines, 0);
     });
   });
 
-  describe("Add trace data if available", function() {
+  describe("Add trace data if available", function () {
     let output, outputLines;
     let trace = {
-      correlationId: "abcd1234"
+      correlationId: "abcd1234",
     };
 
-    before(function() {
+    before(function () {
       process.env.TURBOT_TRACE_CONFIG = JSON.stringify(trace);
     });
 
-    after(function() {
+    after(function () {
       delete process.env.TURBOT_TRACE_CONFIG;
     });
 
-    before(function() {
-      outputLines = testConsole.stdout.inspectSync(function() {
+    before(function () {
+      outputLines = testConsole.stdout.inspectSync(function () {
         log.info("hello");
       });
       output = JSON.parse(outputLines[0]);
     });
-    it("has trace data", function() {
+    it("has trace data", function () {
       assert.deepInclude(output, { trace: trace });
     });
   });
 
-  describe("Hide sensitive data", function() {
+  describe("Hide sensitive data", function () {
     let outputLines, output;
     let data = {
       host: "localhost",
       user: "turbot",
-      $password: "secret"
+      $password: "secret",
     };
-    before(function() {
-      outputLines = testConsole.stdout.inspectSync(function() {
+    before(function () {
+      outputLines = testConsole.stdout.inspectSync(function () {
         log.info("here's the data!", data);
       });
       output = JSON.parse(outputLines[0]);
     });
-    it("host is unchanged (not <sensitive>)", function() {
+    it("host is unchanged (not <sensitive>)", function () {
       assert.equal(output.host, data.host);
     });
-    it("$password is <sensitive>", function() {
+    it("$password is <sensitive>", function () {
       assert.equal(output.$password, "<sensitive>");
     });
   });
 
-  describe("Hide sensitive data", function() {
+  describe("Hide sensitive data", function () {
     let outputLines, output;
     let data = {
       host: "localhost",
       user: "turbot",
-      $password: "secret"
+      $password: "secret",
     };
-    before(function() {
-      outputLines = testConsole.stdout.inspectSync(function() {
+    before(function () {
+      outputLines = testConsole.stdout.inspectSync(function () {
         log.info("here's the data!", data);
       });
       output = JSON.parse(outputLines[0]);
     });
-    it("host is unchanged (not <sensitive>)", function() {
+    it("host is unchanged (not <sensitive>)", function () {
       assert.equal(output.host, data.host);
     });
-    it("$password is <sensitive>", function() {
+    it("$password is <sensitive>", function () {
       assert.equal(output.$password, "<sensitive>");
     });
   });
 
-  describe("circular data is logged", function() {
+  describe("circular data is logged", function () {
     let input, output, outputLines;
-    before(function() {
+    before(function () {
       input = { one: 1, two: 2 };
       input.cycle = input;
-      outputLines = testConsole.stdout.inspectSync(function() {
+      outputLines = testConsole.stdout.inspectSync(function () {
         log.info("I have cyclic data", input);
       });
       output = JSON.parse(outputLines[0]);
     });
-    it("survived intact through both sanitize and JSON.stringify", function() {
+    it("survived intact through both sanitize and JSON.stringify", function () {
       assert.equal(output.one, 1);
     });
   });
 
-  describe("Error objects", function() {
+  describe("Error objects", function () {
     let err, output, outputLines;
-    before(function() {
+    before(function () {
       try {
         throw new Error("my-error");
       } catch (e) {
@@ -133,13 +133,13 @@ describe("turbot-log", function() {
       }
       // console.log(err);
       // console.log(err.stack);
-      outputLines = testConsole.stdout.inspectSync(function() {
+      outputLines = testConsole.stdout.inspectSync(function () {
         log.error("I am an error", err);
       });
       output = JSON.parse(outputLines[0]);
     });
 
-    it("has err object", function() {
+    it("has err object", function () {
       // console.log(output);
       // console.log(output.stack);
       // console.log(output.message);
@@ -147,18 +147,18 @@ describe("turbot-log", function() {
       assert.exists(output.message);
       assert.exists(output.stack);
     });
-    it("has message but the wrapped error message is not elevated, this is a change of behaviour", function() {
+    it("has message but the wrapped error message is not elevated, this is a change of behaviour", function () {
       assert.include(output.message, "my-error");
       assert.include(output.message, "my-wrap");
     });
   });
 
-  describe("Console standard streams per level", function() {
-    before(function() {
+  describe("Console standard streams per level", function () {
+    before(function () {
       process.env.TURBOT_LOG_LEVEL = "debug";
     });
 
-    after(function() {
+    after(function () {
       delete process.env.TURBOT_LOG_LEVEL;
     });
 
@@ -170,52 +170,52 @@ describe("turbot-log", function() {
       ["warning", "stdout"],
       ["notice", "stdout"],
       ["info", "stdout"],
-      ["debug", "stdout"]
+      ["debug", "stdout"],
     ];
 
-    tests.forEach(function(test) {
-      describe(test[0], function() {
+    tests.forEach(function (test) {
+      describe(test[0], function () {
         let stdoutLines, stderrLines;
 
-        before(function() {
-          stderrLines = testConsole.stderr.inspectSync(function() {
-            stdoutLines = testConsole.stdout.inspectSync(function() {
+        before(function () {
+          stderrLines = testConsole.stderr.inspectSync(function () {
+            stdoutLines = testConsole.stdout.inspectSync(function () {
               log[test[0]]("Do I appear in stdout or stderr?");
             });
           });
         });
 
-        it((test[1] == "stderr" ? "does not " : "") + "log to stdout", function() {
+        it((test[1] == "stderr" ? "does not " : "") + "log to stdout", function () {
           assert.lengthOf(stdoutLines, test[1] == "stdout" ? 1 : 0);
         });
-        it((test[1] == "stdout" ? "does not " : "") + "log to stderr", function() {
+        it((test[1] == "stdout" ? "does not " : "") + "log to stderr", function () {
           assert.lengthOf(stderrLines, test[1] == "stderr" ? 1 : 0);
         });
       });
     });
   });
 
-  describe("Logging to an alias", function() {
+  describe("Logging to an alias", function () {
     const tests = [["error", "err"]];
 
-    tests.forEach(function(test) {
-      describe(`${test[1]} -> ${test[0]}`, function() {
+    tests.forEach(function (test) {
+      describe(`${test[1]} -> ${test[0]}`, function () {
         let stdoutLines;
 
-        before(function() {
-          stdoutLines = testConsole.stdout.inspectSync(function() {
+        before(function () {
+          stdoutLines = testConsole.stdout.inspectSync(function () {
             log[test[0]](test[0]);
             log[test[1]](test[1]);
           });
         });
 
-        it(`log.${test[0]} logs with level ${test[0]}`, function() {
+        it(`log.${test[0]} logs with level ${test[0]}`, function () {
           const entry = JSON.parse(stdoutLines[0]);
           assert.equal(entry.level, test[0]);
           assert.equal(entry.message, test[0]);
         });
 
-        it(`log.${test[1]} alias logs with level ${test[0]}`, function() {
+        it(`log.${test[1]} alias logs with level ${test[0]}`, function () {
           const entry = JSON.parse(stdoutLines[1]);
           assert.equal(entry.level, test[0]);
           assert.equal(entry.message, test[1]);
@@ -224,13 +224,13 @@ describe("turbot-log", function() {
     });
   });
 
-  xdescribe("TODO", function() {
-    it("Set log level per trace.logLevel", function() {});
-    it("Logs but does not absorb exceptions", function() {
+  xdescribe("TODO", function () {
+    it("Set log level per trace.logLevel", function () {});
+    it("Logs but does not absorb exceptions", function () {
       let outputLines, output, err;
       // I can't see how to test this ... but it's something like this.
       try {
-        outputLines = testConsole.stderr.inspectSync(function() {
+        outputLines = testConsole.stderr.inspectSync(function () {
           throw new Error("test");
         });
       } catch (e) {
